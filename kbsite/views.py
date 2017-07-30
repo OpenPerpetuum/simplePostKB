58e81123e847a0de560841965d70956710b01707
mikejeffers <mike.s.jeffers@gmail.com>
2017-07-30 19:04:57 -0400

    sort by date

--- kbsite/views.py
+++ kbsite/views.py
@@ -13,8 +13,8 @@ def home(request, pagenum=0):
     total = models.Kill.objects.all().count()
     lower = min(pagenum * step, total)
     upper = min((pagenum + 1) * step, total)
-    kills = models.Kill.objects.all()[lower:upper]
-    return render(request, "main.html", {"kills": kills, "page": pagenum})
+    kills = models.Kill.objects.all().order_by("date")[lower:upper]
+    return render(request, "main.html", {"kills": kills.reverse(), "page": pagenum})
 
 
 def postkill(request):
